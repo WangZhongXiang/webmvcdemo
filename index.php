52293fd9bf3c0f74611fc3154b33ca3c8eed129b
nalan <nalan@mogujie.com>
2015-04-10 22:10:30 +0800

    添加index

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: nalan
+ * Date: 15/4/10
+ * Time: 下午10:07
+ * To change this template use File | Settings | File Templates.
+ */
+echo "welcome to derekjoy's home";
